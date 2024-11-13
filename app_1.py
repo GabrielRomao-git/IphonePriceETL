4db2c8e199a064e51e93edd8c22a2cd7ac91aa37
gabriel-romao <gabrielrromao@outlook.com>
2024-11-12 21:09:33 -0300

    parte 1

--- app_1.py
+++ app_1.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_page():
-    url = 'https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=6&search_layout=stack&type=product&tracking_id=92c2ddf6-f70e-475b-b41e-fe2742459774'
+    url  = 'https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=6&search_layout=stack&type=product&tracking_id=92c2ddf6-f70e-475b-b41e-fe2742459774'
     response = requests.get(url)
     return response.text
 
